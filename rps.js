ab26dba5c81ec7897534c831f2a5ee6a5c8dd867
seif <seif.elmeligy@hotmail.com>
2021-07-06 22:51:24 +0200

    scoring added and it actually works!

--- rps.js
+++ rps.js
@@ -16,34 +16,41 @@ let computerChoice = computerPlay[calculate()].toLowerCase()
 
 // we need to add user interaction and compare it with the computer choice //
 
-let playerChoice = prompt('Welcome to RPS! to play, type Rock, Paper or Scissors').toLowerCase()
+let playerChoice = prompt('Welcome to RPS! to play, type Rock, Paper or Scissors', 'Rock or Paper or Scissors').toLowerCase()
 
 function play() {
     if (playerChoice == "rock" && computerChoice == "paper"){
-        alert('You lose! Paper beats rock!')
+        alert('You lose! Paper beats rock!'); return computerScore +=1
     }
     else if (playerChoice == 'rock' && computerChoice == 'scissors'){
-        alert('You win! Rock beats Scissors!')
+        alert('You win! Rock beats Scissors!'); return playerScore +=1
     }
     else if (playerChoice == "paper" && computerChoice == 'rock'){
-        alert('You lose! Scissors beats Paper!')
+        alert('You Win! Paper beats Rock!'); return playerScore +=1
     }
     else if (playerChoice == "paper" && computerChoice == 'scissors'){
-        alert('You lose! Rock beats Paper!')
+        alert('You lose! Scissors beats Paper!'); return computerScore +=1
     }
     else if (playerChoice == 'scissors' && computerChoice == 'rock'){
-        alert('You lose! Rock beats Scissors!')
+        alert('You lose! Rock beats Scissors!'); return computerScore +=1
     }
     else if (playerChoice == 'scissors' && computerChoice == "paper"){
-        alert('You win! Scissors beat Paper!')
+        alert('You win! Scissors beat Paper!'); return playerScore +=1
     }
     else {
-        alert('Draw! The computer chose the same choice!')}
+        alert('Draw! The computer chose the same choice!')
+    }
 }
+
+// scoring variables testing //
+
+let playerScore = 0;
+let computerScore = 0;
 
 // the base game is now complete, but still needs looping and score keeping // 
 
+console.log(play(), playerScore, computerScore)
 function game() {
-    play()
+    let playerScore = 0;
+    let computerScore = 0;
 }
-
