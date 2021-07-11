6c896ae3e2b06aaa3f3942aecf93d0853203ebce
seif <seif.elmeligy@hotmail.com>
2021-07-11 14:36:20 +0200

    Game fully tested and functional. UI will be implemented in a future update.

--- rps.js
+++ rps.js
@@ -1,63 +1,85 @@
-/* start of project, a variable will be assigned 3 strings and recalled the value randomly */
-
-const computerPlay = ['Rock', 'Paper', 'Scissors'] // scoring variables testing //
-let playerScore = 0;
-let computerScore = 0;
-
-function calculate() {
-  let randomOutcome =  Math.floor(Math.random() * computerPlay.length)
-  return randomOutcome
+let rpsArray = [0, 1, 2];
+function computerPlay (){
+    let rng = Math.floor(Math.random() * rpsArray.length);
+    if (rng === 0){
+        return 'rock'
+    }
+    else if (rng === 1){
+        return 'paper'
+    }
+    else if (rng === 2){
+        return 'scissors'
+    }
 }
- 
-   let computerChoice = computerPlay[calculate()].toLowerCase()
-   let playerChoice = prompt('Welcome to RPS! to play, type Rock, Paper or Scissors', 'Rock or Paper or Scissors').toLowerCase();
-   
-   function computerSelect(){
-       return computerChoice;
-   }
-   function playerSelect(){
-       return playerChoice;
-   }
-
-// adding user interaction and comparing it with the computer choice //
-
-
-
-function play() {
-
-    computerSelect();
-    playerSelect();
-    if (playerChoice == "rock" && computerChoice == "paper"){
-        alert('You lose! Paper beats rock!'); return ++ computerScore 
+function playerPlay(){
+    let choice = prompt('Welcome to a new round of Rock Paper Scissors! To play, type Rock, Paper or Scissors', '')
+    if (choice === '') { 
+        alert('Please type a choice to play a round');
+        playerPlay();
     }
-    else if (playerChoice == 'rock' && computerChoice == 'scissors'){
-        alert('You win! Rock beats Scissors!'); return ++ playerScore 
+    else if (choice.toLowerCase() === 'paper') {
+        return 'paper'
     }
-    else if (playerChoice == "paper" && computerChoice == 'rock'){
-        alert('You Win! Paper beats Rock!'); return ++ playerScore 
+    else if (choice.toLowerCase() === 'scissors') {
+        return 'scissors'
     }
-    else if (playerChoice == "paper" && computerChoice == 'scissors'){
-        alert('You lose! Scissors beats Paper!'); return ++ computerScore 
-    }
-    else if (playerChoice == 'scissors' && computerChoice == 'rock'){
-        alert('You lose! Rock beats Scissors!'); return ++ computerScore 
-    }
-    else if (playerChoice == 'scissors' && computerChoice == "paper"){
-        alert('You win! Scissors beat Paper!'); return ++ playerScore 
+    else if (choice.toLowerCase() === 'rock') { 
+        return 'rock'
     }
     else {
-        alert('Draw! Both you and the computer chose the same choice!');
+        alert('Please type your choice correctly');
+        playerPlay();
+    }}
+function play(){
+    let computerSelection = computerPlay();
+    let playerSelection = playerPlay();
+
+    if (computerSelection === "rock" && playerSelection === "paper"){
+        alert('You win! Paper beats rock!'); playerScore++
     }
-    return scoreAlert;
+    else if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        alert('You lose! Rock beats Scissors!'); computerScore++
+    }
+    else if (computerSelection === "paper" && playerSelection === 'rock'){
+        alert('You lose! Paper beats Rock!');  computerScore++
+    }
+    else if (computerSelection === "paper" && playerSelection === 'scissors'){
+        alert('You win! Scissors beats Paper!'); playerScore++
+    }
+    else if (computerSelection === 'scissors' && playerSelection === 'rock'){
+        alert('You win! Rock beats Scissors!'); playerScore++
+    }
+    else if (computerSelection === 'scissors' && playerSelection == "paper"){
+        alert('You lose! Scissors beat Paper!'); computerScore++
+    }
+    else if (computerSelection===playerSelection) {
+        alert('Draw! Both you and the computer chose the same choice!')    
+    }}
+
+
+
+let computerScore = 0;
+let playerScore = 0;
+
+function scoreAlert(){
+    alert(`Your score is ${playerScore} and computer score is ${computerScore}`);
 }
 
-//console.log(play(), playerScore, computerScore) 
+function game(){
+    play(); scoreAlert();
+    }
 
-let scoreAlert = alert(`Your score is ${playerScore} and the computer score is ${computerScore}`);
+function letsGo (){
+    if (computerScore < 5 && playerScore < 5) {
+    game();
+    letsGo();
+    }
+    else if (playerScore == 5) {
+        alert('Game over! You win!')
+    }
+    else if (computerScore==5){
+        alert('Game over! You lose!')
+    }}
+ 
 
-function game(playerChoice, computerChoice){
-    play(); 
-}
-
-console.log(play())
-    
+letsGo();
